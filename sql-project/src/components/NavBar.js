8ec957162a5523038fb320211f0afd6d2559d8fc
TeeAr00 <t1rate00@students.oamk.fi>
2025-07-07 14:36:12 +0200

    mykistysnappi
    mutenappi navbariin

--- sql-project/src/components/NavBar.js
+++ sql-project/src/components/NavBar.js
@@ -1,20 +1,35 @@
-import React from 'react';
-import { AppBar, Toolbar, Typography, Box, Button, IconButton } from '@mui/material';
+import React, { useState, useEffect } from 'react';
+import { AppBar, Toolbar, Typography, Box, Button, IconButton,Tooltip } from '@mui/material';
 import { Link,useNavigate } from 'react-router-dom';
 import { useTheme } from '@mui/material/styles';
 import ContrastIcon from '@mui/icons-material/Contrast';
 import { useThemeToggle } from './ThemeContext';
-
+import VolumeUpIcon from '@mui/icons-material/VolumeUp';
+import VolumeOffIcon from '@mui/icons-material/VolumeOff';
 
 function NavBar() {
   const navigate = useNavigate();
   const theme = useTheme();
   const { toggleTheme } = useThemeToggle();
 
+  const [muted, setMuted] = useState(() => {
+    const stored = localStorage.getItem('muted');
+    return stored === 'true';
+  })
+
+  useEffect(() => {
+    localStorage.setItem('muted', muted);
+  }, [muted]);
+
   const handleLogout = () => {
     localStorage.removeItem('authToken');
     navigate('/login');
   };
+
+  const toggleMute = () => {
+    setMuted((prev) => !prev);
+  };
+  
   return (
     <AppBar
       position="fixed"
@@ -31,17 +46,32 @@ function NavBar() {
           SQL Laboratory
         </Typography>
         <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
-          <IconButton
-            onClick={toggleTheme}
-            sx={{
-              color: theme.palette.text.primary,
-              '&:hover': {
-                backgroundColor: theme.palette.action.hover,
-              },
-            }}
-          >
-            <ContrastIcon />
-          </IconButton>
+          <Tooltip title={muted ? 'Unmute Sounds' : 'Mute Sounds'}>
+            <IconButton
+              onClick={toggleMute}
+              sx={{
+                color: theme.palette.text.primary,
+                '&:hover': {
+                  backgroundColor: theme.palette.action.hover,
+                },
+              }}
+            >
+              {muted ? <VolumeOffIcon /> : <VolumeUpIcon />}
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Toggle Theme">
+            <IconButton
+              onClick={toggleTheme}
+              sx={{
+                color: theme.palette.text.primary,
+                '&:hover': {
+                  backgroundColor: theme.palette.action.hover,
+                },
+              }}
+            >
+              <ContrastIcon />
+            </IconButton>
+          </Tooltip>
           <Button
             component={Link}
             to="/profile"
